16bbee9ee8ff3876ef420efdcb8b4c658def80f5
CiscG <80540828+CiscG@users.noreply.github.com>
2024-08-26 15:43:44 -0300

    Start

--- Album/src/Album/Main.java
+++ Album/src/Album/Main.java
@@ -1,7 +1,24 @@
 package Album;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Desktop;
+import java.awt.image.BufferedImage;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Desktop d = null;
+        File file = new File(System.getenv("programfiles"));
+
+        /*if(Desktop.isDesktopSupported()){
+            d = Desktop.getDesktop();
+        }
+            try{
+                d.open(file);
+            }
+            catch (IOException e){
+
+            }*/
     }
+
 }
