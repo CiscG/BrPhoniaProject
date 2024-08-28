016198c5acb6f35a34950b6bb5d7d85536dfc15e
CiscG <80540828+CiscG@users.noreply.github.com>
2024-08-28 18:33:11 -0300

    Db funcionando

--- Album/src/Album/DBManager.java
+++ Album/src/Album/DBManager.java
@@ -2,14 +2,15 @@ package Album;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DBManager {
     public Connection connection_to_db(String dbname, String user, String pwrd) {
-        Connection conn = null;
+        Connection connection = null;
         try{
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pwrd);
-            if(conn != null) {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pwrd);
+            if(connection != null) {
                 System.out.println("Connection Established");
             }else{
                 System.out.println("Connection Failed");
@@ -17,6 +18,26 @@ public class DBManager {
         }catch (Exception e){
             System.out.println(e);
         }
-        return conn;
+        return connection;
+    }
+    public void createTable(Connection connection, String table_name){
+        Statement statement;
+        try {
+            String query = "create Table " + table_name + "(employID SERIAL, name varchar(200), adress varchar(200), primary key(employID));";
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Created");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public void insert_photo(Connection connection, String tableName, String name, String adress){
+        Statement statement;
+        try {
+            String query = String.format("insert into %s(name, adress) values('%s', '%s');", tableName, name, adress);
+            System.out.println("Inserted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- Album/src/Album/Main.java
+++ Album/src/Album/Main.java
@@ -1,8 +1,13 @@
 package Album;
 
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         DBManager db = new DBManager();
-        db.connection_to_db("AlbumDB","postgres","123456");
+        Connection connection;
+        connection = db.connection_to_db("AlbumDB","postgres","123456");
+        db.createTable(connection, "test");
+        db.insert_photo(connection, "test", "Gab","Brazil");
     }
 }
