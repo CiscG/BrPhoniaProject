bca4e82497d8bb0cdb6499b57f539840c29c7c4a
CiscG <80540828+CiscG@users.noreply.github.com>
2024-08-29 03:12:26 -0300

    Att

--- Album/src/Album/LoadImage.java
+++ Album/src/Album/LoadImage.java
@@ -1,4 +1,16 @@
 package Album;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class LoadImage {
+    BufferedImage photo;
+    public void loadImage(String path){
+        try {
+            photo = ImageIO.read(DBManager.class.getResource(path));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- Album/src/Album/Main.java
+++ Album/src/Album/Main.java
@@ -5,8 +5,10 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
         DBManager db = new DBManager();
+        LoadImage loadImage = new LoadImage();
         Connection connection = db.connectionToDB("AlbumDB","postgres","123456");
         db.readData(connection,"test");
+        loadImage.loadImage("asdasdasd");
         /*
         db.insertRow(connection, "test", "gasd","Brazil");
         db.updateName(connection, "test", "Gab", "Gabriel");
