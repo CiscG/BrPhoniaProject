de9b012f3d95aac2e5775960ac6d6111cd7fdc3f
CiscG <80540828+CiscG@users.noreply.github.com>
2024-09-02 15:19:05 -0300

    Att

--- Album/src/Album/ShowPhoto.java
+++ Album/src/Album/ShowPhoto.java
@@ -3,37 +3,39 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
+import java.util.Timer;
 
 public class ShowPhoto extends JPanel {
     BufferedImage photo;
+    Timer timer = new Timer();
+    int counter = 5;
+    List<Categories> categories;
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+                if (counter > 0) {
+                    System.out.println(counter + "segundos");
+                    counter--;
+                } else {
+                    System.out.println("Next photo");
+                    timer.cancel();
+                }
+            }
+        };
     public ShowPhoto(){
         setSize(600,400);
         setVisible(true);
 
     }
     public void loadImage(List<Categories> category){
-        //Image image = photo.
+        this.categories = category;
+        ImageIcon icon = new ImageIcon(categories.getFirst().photo);
+        Image image = icon.getImage().getScaledInstance(600, 400, Image.SCALE_SMOOTH);
+        //label.setIcon(new ImageIcon(image));
+    }
+    public void updateImage(){
 
     }
-    public void paintComponent(Graphics graphics){
-        super.paintComponent(graphics);
-        graphics.drawImage(photo, 0, 0, 600, 400, this);
-    }
-    /*public static void main(String[] args){
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame = new JFrame();
-                frame.setSize(600, 400);
-                frame.setVisible(true);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.add(new ShowPhoto());
-                /*
-                Set timer for change photos
-                updateUI();
-            }
-        });
-    }*/
 }
